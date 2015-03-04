5863b89757ee9570bdf880f330ab65211e181d9d
linuxfeng <hw0_feng@163.com>
2015-03-04 16:49:09 +0800

    mv other ubuntu_config

---
+++ ubuntu_config/alias.sh
@@ -0,0 +1,4 @@
+alias amake='ndk-build'
+alias fcpp='find . -type f -name "*.cpp" | xargs grep'
+alias fc='find . -type f -name "*.c" | xargs grep'
+alias fh='find . -type f -name "*.h" | xargs grep'
