313561f7eb994384927ae716fdcb9cdc0c63a37a
linuxfeng <hw0_feng@163.com>
2015-03-04 16:29:43 +0800

    rm linux_kernel_267_work

--- linux_kernel_267_work/include/linux/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 17 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#17 PREEMPT Wed Dec 17 17:02:15 CST 2014"
-#define LINUX_COMPILE_TIME "17:02:15"
-#define LINUX_COMPILE_BY "tyxm"
-#define LINUX_COMPILE_HOST "Aspire"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
