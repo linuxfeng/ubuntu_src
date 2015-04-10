20717736eef55ddd763cf01b0d557a64140b723a
linuxfeng <hw0_feng@163.com>
2015-04-10 13:51:46 +0800

    add pptp协议及android环境mtpd.mkd ,ubuntu_14.04_install_oracle_jdk.mkd, C++_14.mkd.  mv dongtaiku.mkd  动态库的说明及使用.mkd,

--- ubuntu_config/alias.sh
+++ ubuntu_config/alias.sh
@@ -2,4 +2,8 @@ alias amake='ndk-build'
 alias fcpp='find . -type f -name "*.cpp" | xargs grep'
 alias fc='find . -type f -name "*.c" | xargs grep'
 alias fh='find . -type f -name "*.h" | xargs grep'
+alias ff='find . -type f -name '
+alias fd='find . -type d -name '
+######  tmp ######
 alias cg='cat log/A.log | grep fhw > tmp'
+alias as='adb shell '
